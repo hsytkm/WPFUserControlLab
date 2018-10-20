09f83ffe309f74b55353503b2cc1c53b148a82b8
hsytkm <t_hosoya1214@yahoo.co.jp>
2018-10-20 19:19:50 +0900

    プロジェクト ファイルを追加します。

---
+++ 01_AddableList/AddableList/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using AddableList.Views;
+using Autofac;
+using Prism.Autofac;
+using Prism.Modularity;
+using System.Windows;
+
+namespace AddableList
+{
+    class Bootstrapper : AutofacBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var moduleCatalog = (ModuleCatalog)ModuleCatalog;
+            //moduleCatalog.AddModule(typeof(YOUR_MODULE));
+        }
+    }
+}
