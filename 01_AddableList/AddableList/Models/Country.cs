79051aec92dae69e970490b9d2d3e1f304daaf7a
hsytkm <t_hosoya1214@yahoo.co.jp>
2018-10-20 20:27:45 +0900

    とりあえずリスト追加

---
+++ 01_AddableList/AddableList/Models/Country.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace AddableList.Models
+{
+    public class Country
+    {
+        public string Name { get; }
+
+        public string Alias { get; }
+
+        //public IReadOnlyCollection<string> Cities;
+
+        public Country(string name, string alias)
+        {
+            Name = name;
+            Alias = alias;
+        }
+
+    }
+}
--- 01_AddableList/AddableList/ViewModels/MainWindowViewModel.cs
+++ 01_AddableList/AddableList/ViewModels/MainWindowViewModel.cs
@@ -1,18 +1,50 @@
-﻿using Prism.Mvvm;
+﻿using AddableList.Models;
+using Prism.Mvvm;
+using Reactive.Bindings;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace AddableList.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "WPFUserControlLab AddableList";
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
 
+        private ObservableCollection<Country> CountriesSource { get; } = new ObservableCollection<Country>();
+        public ReadOnlyReactiveCollection<Country> Countries { get; }
+
+        public AsyncReactiveCommand AddCommand { get; } = new AsyncReactiveCommand();
+        public AsyncReactiveCommand DelCommand { get; } = new AsyncReactiveCommand();
+
         public MainWindowViewModel()
         {
+            Countries = CountriesSource.ToReadOnlyReactiveCollection();
+
+            AddCommand.Subscribe(async _ =>
+            {
+                await Task.Run(() =>
+                {
+                    CountriesSource.Add(new Country("Japan", "日"));
+                });
+            });
+
+            DelCommand.Subscribe(async _ =>
+            {
+                await Task.Run(() =>
+                {
+                    if (CountriesSource.Count > 1)
+                    {
+                        CountriesSource.RemoveAt(CountriesSource.Count - 1);
+                    }
+                });
+            });
 
         }
     }
